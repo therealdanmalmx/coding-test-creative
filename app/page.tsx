eaec9b6614e1dc61f24ea6a702e3de6f0a35f97e
therealdanmalmx <danmalmx@gmail.com>
2024-06-28 21:21:08 +0100

    Refactor underline

--- app/page.tsx
+++ app/page.tsx
@@ -88,7 +88,7 @@ export default function Home() {
         {quartrOffer.map((offer) => (
           <p
             key={offer.id}
-            className={`cursor-pointer uppercase decoration-4${
+            className={`cursor-pointer uppercase, ${
               offer.title === changeService
                 ? "font-bold text-gray-50 underline underline-offset-4 decoration-4"
                 : "font-normal text-gray-400 no-underline"
